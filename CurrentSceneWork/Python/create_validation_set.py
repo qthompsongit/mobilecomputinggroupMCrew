ed07fbfd515b43a07cfe686b84bc6e28bfffd46a
qthompsongit <qthompson@uchicago.edu>
2024-05-15 12:11:22 -0500

    ks, il data

--- CurrentSceneWork/Python/create_validation_set.py
+++
@@ -1,7 +0,0 @@
-"""
-Write code that takes the labeled data and outputs a training dataset in `Data/Lab2/Train/`
-and a validation dataset in `Data/Lab2/Validation`. These datasets should contain CSV files
-that have their corresponding activity in their filename (similar to the the `Labeled/` folder).
-No file should appear in both the training and validation sets.
-Be sure to include documentation with clear instructions on how to run the script and expected outputs.
-"""
