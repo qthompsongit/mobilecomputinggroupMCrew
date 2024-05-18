629cc63839aa1c302b9a1c33333572cffaf879cf
skarniforous <wowwoomy2@gmail.com>
2024-05-18 18:22:26 -0500

    test

--- python/predict_sensor_trace.py
+++ python/predict_sensor_trace.py
@@ -18,21 +18,32 @@ from sklearn.ensemble import RandomForestClassifier
 import time
 from sklearn.metrics import confusion_matrix
 from sklearn.neural_network import MLPClassifier
+import energyusage
+
+def makepred(n = 10):
+    fp = open("mymodelfile_randfor_2","rb") 
+    myrandfor = pickle.load(fp) 
+    fp.close()
+    mydataset = pd.read_csv(sys.argv[1], index_col=False)
+    print(mydataset.head(5))
+    myprediction = myrandfor.predict_proba(mydataset)
+    print(myrandfor.classes_)
+    arrcheck = [0, 0, 0]
+    predict_val = ['erratic', 'normal', 'semi-erratic']
+    for stats in myprediction:
+        arrcheck[list(stats).index(max(stats))] += 1
+    print(myprediction)
+    print(arrcheck)
+
+    with open("myprediction.txt", "w") as myfile:
+        myfile.write(predict_val[arrcheck.index(max(arrcheck))])
+        
+#energyusage.evaluate(makepred, 10, pdf=False, printToScreen=True)
 
 
-fp = open("mymodelfile_randfor_2","rb") 
-myrandfor = pickle.load(fp) 
-fp.close()
-mydataset = pd.read_csv(sys.argv[1], index_col=False)
-print(mydataset.head(5))
-myprediction = myrandfor.predict_proba(mydataset)
-print(myrandfor.classes_)
-arrcheck = [0, 0, 0]
-predict_val = ['erratic', 'normal', 'semi-erratic']
-for stats in myprediction:
-    arrcheck[list(stats).index(max(stats))] += 1
-print(myprediction)
-print(arrcheck)
+# user function to be evaluated
+def recursive_fib(n):
+    if (n <= 2): return 1
+    else: return recursive_fib(n-1) + recursive_fib(n-2)
 
-with open("myprediction.txt", "w") as myfile:
-    myfile.write(predict_val[arrcheck.index(max(arrcheck))])
+energyusage.evaluate(recursive_fib, 40, pdf=True)
